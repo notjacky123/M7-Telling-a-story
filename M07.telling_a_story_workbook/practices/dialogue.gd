3d71b86ad61efdac94825d054a79b3f940301245
notjacky123 <jalong2@s.sfusd.edu>
2025-10-31 13:59:59 -0700

    done with M7L5

---
+++ M07.telling_a_story_workbook/practices/dialogue.gd
@@ -0,0 +1,28 @@
+extends Control
+
+@onready var rich_text_label: RichTextLabel = %RichTextLabel
+@onready var next_button: Button = %NextButton
+
+var dialogue_items: Array[String] = [
+	"Rose are red",
+	"Violets are blue",
+	"I'm learning about Arrays",
+	"And so are you",
+	]
+	
+var current_item_index := 0
+	
+func show_text() -> void:
+	var current_item := dialogue_items[current_item_index]
+	rich_text_label.text = current_item
+	
+func _ready() -> void:
+	show_text()
+	next_button.pressed.connect(advance)
+	
+func advance() -> void:
+	current_item_index += 1
+	if current_item_index == dialogue_items.size():
+		get_tree().quit()
+	else:
+		show_text()
