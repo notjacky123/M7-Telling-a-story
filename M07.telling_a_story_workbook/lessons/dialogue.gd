1ae2e321ab1f6542da7533b281e764e01158446e
notjacky123 <jalong2@s.sfusd.edu>
2025-11-17 14:42:05 -0800

    M7L11

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -49,6 +49,9 @@ func show_text() -> void:
 	var sound_start_position := randf() * sound_max_offset
 	audio_stream_player.play(sound_start_position)
 	tween.finished.connect(audio_stream_player.stop)
+	
+	slide_in()
+	
 func _ready() -> void:
 	show_text()
 	next_button.pressed.connect(advance)
@@ -59,3 +62,12 @@ func advance() -> void:
 		get_tree().quit()
 	else:
 		show_text()
+	
+func slide_in() -> void:
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_QUART)
+	tween.set_ease(Tween.EASE_OUT)
+	body.position.x = 200.0
+	tween.tween_property(body, "position:x", 0.0, 0.3)
+	body.modulate.a = 0.0
+	tween.parallel().tween_property(body, "modulate:a", 1.0, 0.2)
